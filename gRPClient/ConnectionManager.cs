86f777a5f83f9c9d66a4fc123ad501f3117fa630
Rocketmannnnnnn <j.bonefaas@student.avans.nl>
2022-06-14 14:26:25 +0200

    Added Open, WakeUp and Close functions for the serial communication. Changed target to .net core 3.1.

---
+++ gRPClient/ConnectionManager.cs
@@ -0,0 +1,47 @@
+﻿using gRPCClient;
+using gRPClient.Repo_s;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static gRPClient.Globals;
+
+namespace gRPClient
+{
+    public class ConnectionManager : IWeatherConsoleDAO
+    {
+        private string comport;
+
+        /// <summary>
+        /// TODO remove constructor as it is used as test code
+        /// </summary>
+        public ConnectionManager()
+        {
+            var serialPorts = System.IO.Ports.SerialPort.GetPortNames();
+            Console.WriteLine("Found the following COM ports: ");
+            
+            foreach (var item in serialPorts)
+            {
+                Console.WriteLine(item);
+            }
+            comport = serialPorts[0];
+        }
+
+        public List<WeatherDataPoint> Get()
+        {
+            WeatherStationConnection connection = new WeatherStationConnection();
+            int result = connection.OpenSerialPort(this.comport);
+
+            //Could not open the com port, return null
+            if(result == OK)
+                result = connection.WakeUp();
+
+            //if(result == OK)
+                //result = connection.GetRealTimeData();
+
+            result = connection.CloseSerialPort();
+
+            //TODO implement return value
+            return null;
+        }
+    }
+}
---
+++ gRPClient/WeatherStationConnection.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Text;
+using System.Threading;
+using static gRPClient.Globals;
+
+namespace gRPClient
+{
+    public class WeatherStationConnection
+    {
+        private SerialPort Conn { get; set; }
+
+        /// <summary>
+        /// Opens the serial port for communications
+        /// </summary>
+        /// <returns></returns>
+        public int OpenSerialPort(string comport)
+        {
+            if (comport == null)
+            {
+                Console.WriteLine("OpenSerialPort: Comport name is null!");
+                return NOK;
+            }
+
+            Conn = new SerialPort();
+            Conn.PortName = comport;
+            Conn.BaudRate = 19200;
+            Conn.Open();
+            
+            return OK;
+        }
+
+        /// <summary>
+        /// This function will try to wake the weather station 3 times.
+        /// Every attempt will send a newline character '\n' or hex 0x0A
+        /// and wait 1.2 seconds for a character response.
+        /// When an awnser is given (characters '\n' and '\r') the device is
+        /// awake and standing by.
+        /// </summary>
+        /// <returns></returns>
+        public int WakeUp()
+        {
+            int result = NOK;
+            int idx;
+            char[] ch = new char[1] { (char) 0x0A };
+            char[] resp = new char[2];
+
+            for (idx = 0; idx < 3 && NOK == result; idx++)
+            {
+                Conn.Write(ch, 0, 1);
+
+                Thread.Sleep(1200);
+
+                if (Conn.Read(resp, 0, Conn.BytesToRead) > 0)
+                {
+                    if (resp[0] == '\n' && resp[1] == '\r')
+                    {
+                        result = OK;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"WakeUp: Waking up weather station. Attempt: {idx}");
+                        Thread.Sleep(1200);
+                    }
+                }
+            }
+
+            if (NOK == result)
+                Console.WriteLine("WakeUp: Could not wake up the weather station!");
+
+            return result;
+        }
+
+        public int CloseSerialPort()
+        {
+            if(Conn != null)
+            {
+                this.Conn.Close();
+                return OK;
+            }
+            else
+            {
+                Console.WriteLine("CloseSerialPort: Conn is null. Could not close the serial port!");
+                return NOK;
+            }
+        }
+    }
+}
