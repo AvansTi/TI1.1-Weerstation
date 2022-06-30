709199f7f4c6f5bbfbf9e57bb077f7a232b6028e
Stijn van Houwelingen <stijn@van-houwelingen.net>
2022-06-30 12:15:04 +0200

    added cache

---
+++ Client/Domain/IWeatherDataRequestCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Domain;
+using Shared.Protos;
+
+namespace Client.Domain
+{
+    public interface IWeatherDataRequestCache
+    {
+        void Add(List<ProtoWeatherData> weatherdata);
+        List<ProtoWeatherData> GetAll();
+        void Add(ProtoWeatherData weatherData);
+    }
+}
